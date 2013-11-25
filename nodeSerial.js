e05a80c8650ca683b5995d2a6a0f37e7ffbed8f6
Bill Mills <mills.wj@gmail.com>
2013-11-24 17:43:11 -0800

    first post

---
+++ nodeSerial.js
@@ -0,0 +1,65 @@
+//pull in the packages we need.  first time ever, do: npm install serialport
+var http = require('http'),
+    url = require('url'),
+    childProcess = require('child_process'),
+    serialport = require("serialport"),
+    serialPort = require("serialport").SerialPort,
+    //the name of the serial port you chose in the arduino menu /Tools/Serial Port
+    serialName = '/dev/tty.usbmodem1411',
+    //some variables for later
+    fxnCall, server, data = [], index=0, i, output, queryData,
+    // Create new serialport object, set the baudrate to 9600 for arduino, and define the parser to make sure it knows what a new line is:
+    serial = new serialPort(serialName , { baudrate : 9600, parser: serialport.parsers.readline("\n") }),
+    //give names to the 6 analog in ports here, in order:
+    channelName = [ "'Apollo'",
+                    "'Bacchus'",
+                    "'Cerberus'",
+                    "'Daedalus'",
+                    "'Echo'",
+                    "'Fury'"
+                    ]
+
+
+//wait for the serial connection to open, then start building the output string:
+serial.on("open", function () {
+    //whenever we get some data, write it at the command prompt:
+    serial.on( "data", function( chunk ) {
+        //65535 marks the end of an update; when we get there, build the output string from the stuff we stuck in [data]
+        if(parseInt(chunk,10) != 65535){
+            data[index] = parseFloat(chunk);  //hold the new piece of data in the array
+            index++;                          //step ahead to the next array index
+        } else{
+            output = '({';                    //build a JSON outpt
+            for(i=0; i<data.length; i++){
+                output += channelName[i] + ':' + data[i];
+                if(i!=data.length-1)
+                    output += ',';
+            }
+            output+='})';                     //JSON complete
+            index=0;                          //reset index and data for the next pass
+            data = [];
+        }
+        
+    });
+
+    //let us know if something breaks:
+    serial.on( "error", function( msg ) {
+        output = msg;
+    });
+});
+
+//start a webserver to post the result to the internet:
+server = http.createServer(function (request, response) {
+    queryData = url.parse(request.url, true).query;                 //extract the value of 'callback' from the query string
+
+    fxnCall = childProcess.exec('', function(){
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.end(queryData['callback']+output);                 //post the callback name and output to the browser
+    });
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
---
+++ simpleReader.ino
@@ -0,0 +1,42 @@
+/*
+  ReadAnalogVoltage
+  Reads an analog input on pins 0-5, converts it to voltage, and prints the result to the serial monitor.
+ 
+ This example code is in the public domain.
+ */
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue0 = analogRead(A0);
+  int sensorValue1 = analogRead(A1);
+  int sensorValue2 = analogRead(A2);
+  int sensorValue3 = analogRead(A3);
+  int sensorValue4 = analogRead(A4);
+  int sensorValue5 = analogRead(A5);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage0 = sensorValue0 / 1023.0 * 5.0;
+  float voltage1 = sensorValue1 / 1023.0 * 5.0;
+  float voltage2 = sensorValue2 / 1023.0 * 5.0;
+  float voltage3 = sensorValue3 / 1023.0 * 5.0;
+  float voltage4 = sensorValue4 / 1023.0 * 5.0;
+  float voltage5 = sensorValue5 / 1023.0 * 5.0;
+  
+  unsigned int terminate = 0xFFFFFFFF;
+  // print out the values:
+  Serial.println(voltage0);
+  Serial.println(voltage1);
+  Serial.println(voltage2);
+  Serial.println(voltage3);
+  Serial.println(voltage4);
+  Serial.println(voltage5);
+  Serial.println(terminate);
+  //sample every n milliseconds:
+  delay(1000);
+}
